47be00551f075bdec4beb044a0baa1903601b23d
Naimur Rahman <naimur.rahman900@gmail.com>
2025-01-08 14:55:20 +0600

    navbarToolWorksNicely

--- src/components/builder/sections/HeaderSection.tsx
+++ src/components/builder/sections/HeaderSection.tsx
@@ -220,7 +220,7 @@ export const HeaderSection = ({
             : layout === "between"
             ? "justify-between"
             : ""
-        } bg-[${color}] items-center ${headerStyle.className}`}
+        } items-center ${headerStyle.className}`}
         style={styles.background || {}}
       >
         <ShowLogo />
